9bbc444091b9c20eaeb95921b19ca70c18c86b6b
cchvn <92581528+cchvn@users.noreply.github.com>
2022-11-17 15:20:58 +0100

    Update ContextRetrieval.py

--- ContextRetrieval.py
+++ ContextRetrieval.py
@@ -17,10 +17,12 @@ seed_value = 42
 np.random.seed(seed_value)
 
 # Create the dataframe
+# Reading the json file
 with open("./squad1.1/train-v1.1.json", "r") as f:
     json_file = json.load(f)
 data = json_file["data"]
 
+# Organizing the data to use them easily
 rows = []
 for document in data:
   for par in document['paragraphs']:
@@ -39,13 +41,14 @@ df = pd.DataFrame(rows)
 print('Dataframe created')
 
 # Preprocessing of the data
+# With this function we remove the line breaks, and all the ponctuation, moreover, we tranform all the upper case letters in lower case.
 def preprocess_text(text):
     REPLACE_WITH_SPACE = re.compile(r"\n") 
     text = [REPLACE_WITH_SPACE.sub(" ", line) for line in text]
     text = [re.sub(r"([(.;:!\'ˈ~?,\"(\[\])\\\/\-–\t```<>_#$€@%*+—°′″“”×’^₤₹‘])", r'', line) for line in text]
-    return text
+    return text.str.lower()
 
-# Creating a copy of the original dataframe (we do this because we want to be able to compare the results of our processing with the original data)
+# Creating a copy of the original dataframe (we want to compare the results of our model with the original data)
 df_preprocess = df.copy()
 
 # pre-process context and question text
@@ -53,14 +56,10 @@ df_preprocess['context'] = preprocess_text(df['context'])
 df_preprocess['question'] = preprocess_text(df['question'])
 df_preprocess['answer_text'] = preprocess_text(df['answer_text'])
 
-df_preprocess["context"]=df_preprocess["context"].str.lower()
-df_preprocess["question"]=df_preprocess["question"].str.lower()
-df_preprocess["answer_text"]=df_preprocess["answer_text"].str.lower()
-
 print('Preprocess done')
 
 # Train/Test Split
-split_value = 0.1 #@param {type:"number"} 
+split_value = 0.1 #we take 10% of data as test data because the last step to compute recall is long to compute
 test_dim = int(len(df['title'].unique()) * split_value)
 test_titles = np.random.choice(df['title'].unique(), size=test_dim, replace=False)
 
@@ -69,7 +68,6 @@ df_test = df_preprocess[df_preprocess['title'].isin(test_titles)]
 df_train = df_preprocess[~(df_preprocess['title'].isin(test_titles))]
 
 df_original_test = df[df['title'].isin(test_titles)]
-context_test = df_original_test['context'].unique() 
 
 print('Train/test split done')
 
@@ -82,8 +80,10 @@ context_prep_train = df_train['context'].unique().tolist()
 # document-term matrix on the training set
 context_tf_idf = vectorizer.fit_transform(context_prep_train)
 
-# obtain unique contexts from the test set
-context_prep_test = df_test['context'].unique().tolist() # preprocessed
+# obtain unique contexts from the test set because different questions can have the same context. We do that on both preprocessed and original data in 
+# order to compare our results with the ground truth (original data) at the end.
+context_prep_test = df_test['context'].unique().tolist() 
+context_test = df_original_test['context'].unique() #
 
 # transform test contexts and questions
 context_test_tf_idf = vectorizer.transform(context_prep_test)
@@ -94,15 +94,14 @@ results = cosine_similarity(context_test_tf_idf, question_tf_idf)
 execution_time = time.time()-start
 print('Context retrieval done in ', execution_time, ' seconds')
 
-# Evaluate our model
-
+# Evaluate our model :
 # In this cell we evaluate how effective is tf-idf in finding the right context for a question : To do so we compute 
 # the result recall for each k between 1 and k_results: in particular, for each question we take the k most 
 # probable contexts and check if the right context is among them 
 
 k_results = 10  
 
-tp = 0 #true positives
+tp = 0 #number of true positives
 recall = []
 for k in range(1, k_results+1):
     for j,result in enumerate(results.T):
@@ -119,9 +118,8 @@ print('----------------------------')
 for i in range(0,len(recall)):
     print('Recall for k = ', i, ' : ', recall[i], '\n')
 
+# plot and export the recall in terms of k
 plt.plot(list(range(1, k_results+1)),recall,"-s");
 plt.xlabel("k");
 plt.ylabel("recall");
-
 plt.savefig('recall.png')
-
